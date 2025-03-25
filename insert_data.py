a6b43fc9f363bb5c4f1d181856f761299b5807a9
LucasHenriqueAzv <lucashenrique.azv@gmail.com>
2025-03-25 09:29:28 -0300

    criando views

--- insert_data.py
+++ insert_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 from os import getenv
 
@@ -17,3 +17,37 @@ engine = create_engine(connection_string)
 dados_inseridos = dataset.to_sql('temperature_readings', engine, if_exists='replace', index=False)
 
 print(f'foram inseridos {dados_inseridos} dados')
+
+queries_views = [
+    # media de temperatura agrupada por sensor (In ou Out)
+    """
+    CREATE OR REPLACE VIEW avg_temp_por_local AS
+    SELECT "out/in" AS sensor_location, AVG(temp) AS avg_temp
+    FROM temperature_readings
+    GROUP BY "out/in";
+    """,
+    # contagem de leituras por hora (a partir do noted_date)
+    """
+    CREATE OR REPLACE VIEW leituras_por_hora AS
+    SELECT EXTRACT(HOUR FROM to_timestamp(noted_date, 'DD-MM-YYYY HH24:MI')) AS hora,
+           COUNT(*) AS contagem
+    FROM temperature_readings
+    GROUP BY hora
+    ORDER BY hora;
+    """,
+    # temperaturas max e min / dia (convertendo noted_date para formato de data)
+    """
+    CREATE OR REPLACE VIEW temp_max_min_por_dia AS
+    SELECT to_char(to_timestamp(noted_date, 'DD-MM-YYYY HH24:MI'), 'YYYY-MM-DD') AS data,
+           MAX(temp) AS temp_max,
+           MIN(temp) AS temp_min
+    FROM temperature_readings
+    GROUP BY data
+    ORDER BY data;
+    """
+]
+
+with engine.begin() as connection:
+    for query in queries_views:
+        connection.execute(text(query))
+        print("view criada")
